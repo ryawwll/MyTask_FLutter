12475e6e3cfdbe94cbd95dbb882a05bcfa150cf3
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-05-14 13:58:16 +0700

    Baru lagi

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:lapanganku/app/presentation/login_screen.dart';
 import 'package:lapanganku/app/presentation/register_screen.dart';
 import 'package:lapanganku/app/presentation/splash_screen.dart';
+import 'package:hive/hive.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.openBox('user');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
